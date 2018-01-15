c4f1a2d29602b59b715b6b312bb3d122a0a66118
Danny <dannytran01@gmail.com>
2018-01-14 17:05:44 -0800

    Intercepted all request with .jpg and changed its response to a .gif instead

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,11 @@
 self.addEventListener('fetch', function(event) {
-  // TODO: only respond to requests with a
-  // url ending in ".jpg"
-  event.respondWith(
-    fetch('/imgs/dr-evil.gif')
-  );
+  	// TODO: only respond 'to requests with a
+  	// url ending in ".jpg
+  	let url = event.request.url;
+
+	if(url.substr(url.length - 4) === ".jpg"){
+  		event.respondWith(
+  			fetch('/imgs/dr-evil.gif'));
+  	}
+
 });
