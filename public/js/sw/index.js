f6896eb35ab869d3f3e58198a07199a70941a48b
dtran <dtran@adaptivebiotech.com>
2018-01-15 22:16:58 -0800

    Opened a cache in the install, set up the event to add all the url of resources to include in the cache

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -7,11 +7,11 @@ self.addEventListener('install', function(event) {
     'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
     'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
   ];
-
+  
   event.waitUntil(
-    // TODO: open a cache named 'wittr-static-v1'
-    // Add cache the urls from urlsToCache
-  );
+      caches.open('wittr-static-v1').then(cache =>
+        cache.addAll(urlsToCache)
+  ));
 });
 
 self.addEventListener('fetch', function(event) {
