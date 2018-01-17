b53cc2e40620f8d8e09a2baa099886bdfc8d129b
dtran <dtran@adaptivebiotech.com>
2018-01-16 17:39:36 -0800

    Attempt to fetch from all cache a specific request, if undefined, then attempt a network fetch

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -16,4 +16,13 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   // TODO: respond with an entry from the cache if there is one.
   // If there isn't, fetch from the network.
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            if(response){
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+
 });
